7cc9ddcc275f6f4773ee1cfc7ed5615c1d48f0a9
clevermiraz <miraz.onesoft@gmail.com>
2025-10-15 08:57:01 +0200

    04: authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
-import { getQueryClient, trpc } from '@/trpc/server';
-import { Client } from './client';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import { Suspense } from 'react';
+import { requireAuth } from '@/lib/auth-utils';
+import { caller } from '@/trpc/server';
 
 const Page = async () => {
-  const queryClient = getQueryClient();
+  await requireAuth();
 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  const data = await caller.getUsers();
 
   return (
     <div
-      className='
-      flex flex-col items-center justify-center h-screen'
+      className='min-h-screen min-w-screen
+      flex flex-col items-center justify-center gap-y-6'
     >
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+      Protected Server component <br />
+      <div>{JSON.stringify(data, null, 2)}</div>
     </div>
   );
 };
